c98524d308f19a17300c6fee7f5e07192961fa29
annuszka <annrybak92@gmail.com>
2019-01-11 00:52:15 +0100

    added testing class publicrecipes to display query in about page with only public recipes

--- Przepisnik/Controllers/HomeController.cs
+++ Przepisnik/Controllers/HomeController.cs
@@ -3,17 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Przepisnik.ViewModels;
+using Przepisnik.Data;
+using System.Collections;
 
 namespace Przepisnik.Controllers
 {
     public class HomeController : Controller
     {
+        private RecipesDBContext db = new RecipesDBContext();
+
         public ActionResult Index()
         {
             return View();
+
+        }
+        public ActionResult About()
+        {
+            IQueryable<PublicRecipes> query = from recipe in db.Recipes
+                                              where recipe.IfPublic == true
+                                              select new PublicRecipes()
+                                              {
+                                                  PublicID = recipe.RecipeID,
+                                                  Image = recipe.RecipePhotoUrl,
+                                                  PreparationT = recipe.PrepTime,
+                                                  RecipeTitle = recipe.Title,
+                                                  Descr = recipe.Description
+                                              };
+        
+            return View(query.ToList());
+        }
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
 
-        public ActionResult About()
+        public ActionResult AboutRecipes()
         {
             ViewBag.Message = "Your application description page.";
 
---
+++ Przepisnik/ViewModels/PublicRecipes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Przepisnik.ViewModels
+{
+    public class PublicRecipes
+    {
+        public int PublicID { get; set; }
+        public string PreparationT { get; set; }
+        public string RecipeTitle { get; set; }
+        public string Descr { get; set; }
+        public string Image { get; set; }
+    }
+}
